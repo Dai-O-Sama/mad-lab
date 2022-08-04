9eda45841ffb16f11d2afe659303003ea2afabf4
cronesis <100788377+cronesis@users.noreply.github.com>
2022-08-04 13:10:42 +0530

    Update MainActivity1.java

--- program1/MainActivity1.java
+++ program1/MainActivity1.java
@@ -1,45 +1,25 @@
 package com.example.program1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("lifecycle","onCreate invoked");
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("lifecycle","onStart invoked");
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("lifecycle","onResume invoked");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("lifecycle","onPause invoked");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("lifecycle","onStop invoked");
-    }
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("lifecycle","onRestart invoked");
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("lifecycle","onDestroy invoked");
+
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(view -> {
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
